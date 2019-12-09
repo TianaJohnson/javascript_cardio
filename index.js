1576461c332e2f0faa702671acadd495d59da364
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-09 16:21:36 -0600

    fizz

--- index.js
+++ index.js
@@ -162,7 +162,15 @@ function reverseString(str) {
   
   // CHALLENGE 6: FIZZBUZZ
   // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-  function fizzBuzz() {}
+  function fizzBuzz() {
+      for( let i = 1; i <= 100; i ++) {
+          if(i % 3 === 0) {
+              console.log('fizz');
+          } else {
+              console.log(i);
+          }
+      }
+  }
   
   
 
@@ -236,6 +244,8 @@ function reverseString(str) {
  // const output = isPalindrome('racecar');
  // const output = reverseInt('-01234');
 //  const output = capitalizeLetters('i love javascript')
-    const output = maxCharacter('javascript');
+    // const output = maxCharacter('javascript');
+    const output = fizzBuzz();
+  
   
   console.log(output);
