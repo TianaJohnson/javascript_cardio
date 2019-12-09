1599d4d1aa83587d4c7b253ff233de0ee78855cd
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-08 19:54:09 -0600

    reduced and for loop

--- index.js
+++ index.js
@@ -180,6 +180,37 @@ function reverseString(str) {
 
 // console.log(largest);
 // console.log(smallest);
+
+///////////////////
+
+//3rd
+
+// const array = [37,-5,-15,-37,5,15]
+
+// const forLoopMinMax = () => {
+//   let min = array[0], max = array[0]
+
+//   for (let i = 1; i < array.length; i++) {
+//     let value = array[i]
+//     min = (value < min) ? value : min
+//     max = (value > max) ? value : max
+//   }
+
+//   return [min, max]
+// }
+
+// const [forLoopMin, forLoopMax] = forLoopMinMax()
+// console.log(`Minimum: ${forLoopMin}, Maximum: ${forLoopMax}`) // Minimum: -37, Maximum: 37
+
+///////
+
+//4th, reduced
+
+const array = [37,-5,-15,-37,5,15]
+
+const minUsingReduce = () => array.reduce((min, currentValue) => Math.min(min, currentValue), array[0])
+const maxUsingReduce = () => array.reduce((max, currentValue) => Math.max(max, currentValue), array[0])
+console.log(`Minimum: ${minUsingReduce()}, Maximum: ${maxUsingReduce()}`) // Minimum: -37, Maximum: 37
   
    // Call Function
  // const output = reverseString('hello');
