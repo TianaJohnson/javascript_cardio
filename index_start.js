ee157108890a2183470b8e4c40937367c7392fc8
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-11 20:19:22 -0600

    session 2

---
+++ index_start.js
@@ -0,0 +1,7 @@
+// CHALLENGE 1: LONGEST WORD
+// Return the longest word of a string
+// ex. longestWord('Hi there, my name is Brad') === 'there,'
+
+function longestWord(sen) {
+const wordArr = sen.toLowerCase().match();
+  }
