c333a12a24c1814c697126aec82cb39422365329
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-11 20:30:21 -0600

    regular expretion added

--- index.js
+++ index.js
@@ -253,8 +253,24 @@ function reverseString(str) {
 //  const output = capitalizeLetters('i love javascript')
     // const output = maxCharacter('javascript');
     //const output = fizzBuzz();
-    console.log(output);
+    //console.log(output);
 
     //////////////////////////////////////////////////////////////
 
     /////////////////// SESSION 2 ////////////////////////////////
+    
+// CHALLENGE 1: LONGEST WORD
+// Return the longest word of a string
+// ex. longestWord('Hi there, my name is Brad') === 'there,'
+
+function longestWord(sen) {
+    //regular expretion example
+const wordArr = sen.toLowerCase().match(/[ a-z0-9 ]+/g);   //regular exprection goes between // a-z 0-9
+                                                            //g = global, meaning it doesnt stop at the first match, it keeps going
+    console.log(wordArr);
+  }
+
+  // Call Function
+const output = longestWord('Hello, my name is Brad');
+
+console.log(output);
