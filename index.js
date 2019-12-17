489398247501ad84b990b50cd6ea8b94f7cec956
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-16 19:00:04 -0600

    cleaned up

--- index.js
+++ index.js
@@ -351,17 +351,15 @@ function formatStr(str) {
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
 function letterChanges(str)  {
-    let newStr = str.toLowerCase().replace(/[a-z]/gi, function(char) {
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, char => {
         if(char === 'z' || char === 'Z') {
             return 'a';
         } else {
             return String.fromCharCode(char.charCodeAt() +1);
         }
     });
-    newStr = newStr.replace(/a|e|i|o|u/gi, function(vowel) {
-
-        return vowel.toUpperCase();
-    });
+    newStr = newStr.replace(/a|e|i|o|u/gi, vowel => vowel.toUpperCase());
+    
     return newStr;
 
 }
